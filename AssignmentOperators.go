eaf1d4f429bff80fda8eaca8f97b0fb360523de1
Anulika Banerjee <anu.banerjee08@gmail.com>
2018-09-13 09:37:31 +0530

    Basics- assignment operators

---
+++ AssignmentOperators.go
@@ -0,0 +1,16 @@
+/* variable declaration and initialization*/
+
+package main
+
+import . "fmt"
+const VALUE int = 10	//constants are declared outside functions. It's good practice to name them in all-caps.
+func main(){
+	var a int = 3
+	var d int	//have to declare dataType else errors out
+	d=5
+	var c = 4	// automatic dataType assigned as int
+	b := 1		// := is used for short variable declarations+assignment. This is same as var b = 1
+	Println(a+b+c+d+VALUE)
+
+}
+
